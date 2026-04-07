d8ebbbcbfbb2c13ef698aba31751e76d5dbd627c
kvnprdtyaa <kevinpradityabussines@gmail.com>
2026-04-07 17:00:16 +0700

    feat: add quote export to CSV and JSON
    - update dependencies in Cargo.toml and Cargo.lock
    - add generated quotes.csv and quotes.json outputs
    - update main scraping logic in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use reqwest;
 use scraper::{Html, Selector};
-use std::error::Error;
+use serde::Serialize;
+use std::{error::Error, fs::File};
+
+#[derive(Debug,Serialize)]
+struct Quote {
+    text: String,
+    author: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+      let mut page = 1;
     let url = "https://quotes.toscrape.com/";
 
     println!("Fetching data from the: {}", url);
@@ -16,15 +24,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let text_selector = Selector::parse(".text").unwrap();
     let author_selector = Selector::parse(".author").unwrap();
 
-    let mut quotes = Vec = Vec::new();
+    let mut quotes: Vec<Quote> = Vec::new();
 
-    for (index, quote) in document.select(&quote_selector).enumerate() {
+    for quote in document.select(&quote_selector) {
         let text = quote.select(&text_selector).next().map(|t| t.text().collect::<Vec<_>>().join("")).unwrap_or_default();
         let author = quote.select(&author_selector).next().map(|t| t.text().collect::<Vec<_>>().join("")).unwrap_or_default();
 
-        println!("\n{}. {} - {}", index + 1, text, author);
+        quotes.push(Quote { text, author });
     }
-    println!("\n");
+
+    println!("Scraped {} quotes.", quotes.len());
+
+    save_to_json(&quotes)?;
+
+    save_to_csv(&quotes)?;
+
+    println!("\nData saved to quotes.json and quotes.csv\n");
+    Ok(())
+}
+
+fn save_to_json(quotes: &Vec<Quote>) -> Result<(), Box<dyn Error>> {
+    let file = File::create("quotes.json")?;
+    
+    serde_json::to_writer_pretty(file, quotes)?;
+    Ok(())
+}
+
+fn save_to_csv(quotes: &Vec<Quote>) -> Result<(), Box<dyn Error>> {
+    let mut writer = csv::Writer::from_path("quotes.csv")?;
+
+    for quote in quotes {
+        writer.serialize(quote)?;
+    }
+
+    writer.flush()?;
 
     Ok(())
 }
